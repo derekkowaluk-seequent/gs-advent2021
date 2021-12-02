032ab5a9ab0fcba9a8474dd00543f2f0639e8c3e
Derek Kowaluk <derek.kowaluk@seequent.com>
2021-12-02 08:10:25 -0700

    Add Skeleton

---
+++ day1/solutions/derekkowaluk/day1.py
@@ -0,0 +1,17 @@
+# Advent 2021 Day 1
+# Derek Kowaluk Dec 1, 2021
+# 
+
+
+import sys
+import os
+
+
+def main():
+
+
+	solution = 0
+	print("The Solution is {}".format(solution))
+
+if __name__ == '__main__':
+	main()
