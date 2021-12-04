624cf41e55f5604fbb170d8433d025672376e490
derekkowaluk-seequent <90342284+derekkowaluk-seequent@users.noreply.github.com>
2021-12-04 13:26:24 -0700

    Derek Kowaluk Solution for Day 1 (#2)
    * Start Day 1
    
    * Add Skeleton
    
    * Add File Opening
    
    * Hook Up
    
    * derek python solution complete
    
    * Minor update
    
    * Add helper function get_data, stub in solve_part2
    
    * Update solve_part1 to use get_data func
    
    * Clean up, prepare for part2 solution
    
    * First pass solve_part2
    
    * Add example data for testing, fix output to match problem text
    
    * Use example data if file not supplied
    
    * Move derek's input to derek's solution

--- day1/solutions/derekkowaluk/day1.py
+++ day1/solutions/derekkowaluk/day1.py
@@ -7,44 +7,111 @@ import sys
 import os
 
 
-def solve(filename):
+def get_example_data():
+	return [
+		199,
+		200,
+		208,
+		210,
+		200,
+		207,
+		240,
+		269,
+		260,
+		263
+		]
+
+def get_data(filename):
 	f = None
+	data = []
+
 	try:
 		f = open(filename)
 	except OSError:
 		print("File Error")
-		return 0
 
-	increase_count = 0
-	decrease_count = 0
+		return [-1, data]
+
 	line = f.readline()
-
-	previous = value = 0 + int(line)
-
 	while line:
 		value = 0 + int(line)
-		if (value > previous):
-			print("{} (increased)".format(value))
-			increase_count = increase_count + 1
-		elif (value < previous):
-			print("{} (decreased)".format(value))
-			decrease_count = decrease_count + 1
-		else:
-			print("{} (unchanged)".format(value))
-		previous = value
+		data.append(value)
 		line = f.readline()
+	return [0,data]
+
+
+def solve_part2(data):
+	if len(data) < 3 : return 0
+	increase_count = 0
+	decrease_count = 0
+
+	remove_index = 0
+	index = 2
+
+	previous = 0
+	eachvalue = data[0] + data[1]
+	first = True
+	while index < len(data):
+		eachvalue = eachvalue + data[index]
+		if first: 
+			print("{} (N/A - no previous sum)".format(eachvalue))
+			first = False
+		else:
+			if (eachvalue > previous):
+				print("{} (increased)".format(eachvalue))
+				increase_count = increase_count + 1
+			elif (eachvalue < previous):
+				print("{} (decreased)".format(eachvalue))
+				decrease_count = decrease_count + 1
+			else:
+				print("{} (no change)".format(eachvalue))
+		previous = eachvalue
+		eachvalue = eachvalue - data[remove_index]
+		remove_index = remove_index + 1
+		index = index + 1
+
+	return increase_count
+
+def solve_part1(data):
+	increase_count = 0
+	decrease_count = 0
+	previous = 0
+	first = True
+
+	for eachvalue in data:
+		if first: 
+			print("(N/A - no previous measurement)")
+			first = False
+		else:
+			if (eachvalue > previous):
+				print("{} (increased)".format(eachvalue))
+				increase_count = increase_count + 1
+			elif (eachvalue < previous):
+				print("{} (decreased)".format(eachvalue))
+				decrease_count = decrease_count + 1
+			else:
+				print("{} (no change)".format(eachvalue))
+		previous = eachvalue
 
 	print(decrease_count)
 	return increase_count
 
 def main():
+	data = None
 	if len(sys.argv) < 2: 
-		print("File Path Required")
-		sys.exit()
-	filepath = sys.argv[1]
+		print("Using Example Data")
+		data = get_example_data()
+	else:
+		filepath = sys.argv[1]
+		result,data = get_data(filepath)
+		if result : 
+			sys.exit()
 
-	solution = solve(filepath)
-	print("The Solution is {}".format(solution))
+	solution = solve_part1(data)
+	print("The Solution for part 1 is {}".format(solution))
+
+	solution = solve_part2(data)
+	print("The Solution for part 2 is {}".format(solution))
 
 if __name__ == '__main__':
 	main()
