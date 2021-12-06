c9ceee5a44d545bf573ae938b198d23081c7cade
derekkowaluk-seequent <90342284+derekkowaluk-seequent@users.noreply.github.com>
2021-12-05 22:17:09 -0700

    Derek Kowaluk Solution for Day 3 (#4)
    * Add Input for Day2
    
    * Add Skeleton for day2.py
    
    * Start solve part1
    
    * Add Some Formatting, options to not show calc output
    
    * Solution for Part1 Complete
    
    * part 2 solution
    
    * Show hpos, dpos and aim
    
    * Day 2 Solution Complete
    
    * Add day3.py and input
    
    * Strip Input lines of whitespace
    
    * start 3 part 2
    
    * Start Filter Oxygen
    
    * Oxygen Filter Working
    
    * Oxygen Filter Working
    
    * Oxygen Filter Reduced
    
    * Add bin to dec func, start c02 code
    
    * Fix Compare filter for C02
    
    * Add showoutput support

---
+++ day3/derekkowaluk/day3.py
@@ -0,0 +1,149 @@
+# Advent 2021 Day 3
+# Derek Kowaluk Dec 4, 2021
+# 
+
+import sys
+import os
+
+def get_example_data():
+	return [
+		"00100",
+		"11110",
+		"10110",
+		"10111",
+		"10101",
+		"01111",
+		"00111",
+		"11100",
+		"10000",
+		"11001",
+		"00010",
+		"01010"
+		]
+
+def get_data(filename):
+	f = None
+	data = []
+	try:
+		f = open(filename)
+	except OSError:
+		print("File Error")
+		return [-1, data]
+
+	line = f.readline()
+	while line:
+		data.append(line.strip())
+		line = f.readline()
+	return [0,data]
+
+
+def filter_data(data, filter, compare):
+	a_data = []
+	b_data = []
+	for eachdata in data:
+		if filter(eachdata): a_data.append(eachdata)
+		else: b_data.append(eachdata)
+
+	if compare(a_data,b_data):
+		return a_data
+	return b_data
+
+
+def bin_to_dec(value):
+	result = 0
+	for eachbit in value:
+		result = result << 1
+		if eachbit == '1': result = result + 1
+
+	return result
+
+def solve_part2(data, showoutput = True):
+	oxygen = 0
+	CO2 = 0
+
+	oxy_data = data.copy()
+	pos = 0
+	longest = 0
+	while len(oxy_data) > 1:
+		oxy_data = filter_data(oxy_data, lambda v:v[pos] == '1', lambda a, b: len(a) >= len(b))
+		if showoutput : print(oxy_data)
+		pos = pos + 1
+		if pos > longest : longest = pos
+
+	oxygen = bin_to_dec(oxy_data[0])
+	print("Oxygen:{}".format(oxygen))
+	
+	co2_data = data.copy()
+	pos = 0
+	longest = 0
+	while len(co2_data) > 1:
+		co2_data = filter_data(co2_data, lambda v:v[pos] == '1', lambda a, b: len(a) < len(b))
+		if showoutput :print(co2_data)
+		pos = pos + 1
+		if pos > longest : longest = pos
+
+	CO2 = bin_to_dec(co2_data[0])
+	print("CO2:{}".format(CO2))
+
+
+	return oxygen * CO2
+
+def solve_part1(data, showoutput = True):
+	gamma = 0
+	epsilon = 0
+	results = []
+	longest = 0
+	for val in range(32): results.append(0) 
+	for eachvalue in data:
+		pos = 0
+		for eachbit in eachvalue:
+			if int(eachbit) == 0: results[pos] = results[pos] - 1
+			else : results[pos] = results[pos] + 1
+			pos = pos + 1
+			if pos > longest : longest = pos
+
+	gammastr = ""
+	epsilonstr = ""
+
+	for index in range(longest):
+		gamma = gamma << 1
+		epsilon = epsilon << 1
+
+		if results[index] > 0 : 
+			gammastr = gammastr + '1'
+			epsilonstr = epsilonstr + '0'
+			gamma = gamma + 1
+		else : 
+			gammastr = gammastr + '0'
+			epsilonstr = epsilonstr + '1'
+			epsilon = epsilon + 1
+
+
+
+	print("{}:{}".format(gammastr, gamma))
+	print("{}:{}".format(epsilonstr, epsilon))
+
+
+	return gamma * epsilon
+
+def main():
+	data = None
+	if len(sys.argv) < 2: 
+		print("Using Example Data")
+		print("------------------")
+		data = get_example_data()
+	else:
+		filepath = sys.argv[1]
+		print("Using file:{}".format(filepath))
+		result,data = get_data(filepath)
+		if result : 
+			sys.exit()
+
+	solution = solve_part1(data, False)
+	print("The Solution for part 1 is {}".format(solution))
+
+	solution = solve_part2(data, False)
+	print("The Solution for part 2 is {}".format(solution))
+
+if __name__ == '__main__':
+	main()
