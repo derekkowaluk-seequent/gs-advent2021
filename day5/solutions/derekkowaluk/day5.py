46254ff1a7671f920d9b4a5b1b1d3dd17aa9c293
Derek Kowaluk <derek.kowaluk@seequent.com>
2021-12-09 10:28:12 -0700

    Solve for example

--- day5/solutions/derekkowaluk/day5.py
+++ day5/solutions/derekkowaluk/day5.py
@@ -50,6 +50,48 @@ def print_line(ldata):
 	ldata[1][0], ldata[1][1]
 	))
 
+def get_chart(ldata, xsize = 10, ysize = 10):
+	mem = [0] * xsize * ysize
+	return mem
+
+def print_chart(mem, xsize = 10, ysize = 10):
+	for x in range(xsize):
+		for y in range(ysize):
+			v = mem[x * xsize + y]
+			print((str(v) if v else '.'),end='')
+		print()
+	print()
+
+def draw_point(mem, point):
+	#print(point)
+	loc = point[1] * 10 + point[0]
+	#print(loc)
+	mem[loc] = mem[loc] + 1
+
+def draw_line(mem, line):
+	#print(line)
+	dx = line[1][0] - line[0][0]
+	dy = line[1][1] - line[0][1]
+	#print("dx:{} dy{}".format(dx,dy))
+	x0, y0 = line[0]
+	xstep = 1 if dx >= 0 else -1
+	ystep = 1 if dy >= 0 else -1
+	mx = abs(dx)
+	my = abs(dy)
+	yaccum = 0
+	ny = 0
+
+	if mx == 0 :
+		
+		for y in range(y0, y0 + dy + ystep, ystep):
+			draw_point(mem, [x0,y])
+	else:
+		for x in range(x0, x0+dx+xstep,xstep):
+			draw_point(mem, [x,y0 + ny])
+			yaccum = yaccum + my
+			while yaccum >= mx:
+				ny = ny + ystep
+				yaccum = yaccum - mx
 
 def solve_part2(data, showoutput = True):
 	return 0
@@ -58,12 +100,19 @@ def solve_part1(data, showoutput = True):
 
 	line_data = [extract_points(l) for l in data[:11]]
 
-	for each in line_data:
-		print_line(each)
+	mem = get_chart(line_data)
 
+	for each in line_data[0:10]:
+		if each[0][0] == each[1][0] or each[0][1] == each[1][1]:
+			draw_line(mem, each)
 
+	overlap_count = 0
+	for each in mem:
+		if each > 1 : overlap_count = overlap_count + 1
 
-	return 0
+	print_chart(mem)
+
+	return overlap_count
 
 def main():
 	data = None
