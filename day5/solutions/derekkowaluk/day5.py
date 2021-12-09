cb931178933425624e7b54f799a5c084c937d9f0
Derek Kowaluk <derek.kowaluk@seequent.com>
2021-12-09 07:48:55 -0700

    Start day5

---
+++ day5/solutions/derekkowaluk/day5.py
@@ -0,0 +1,66 @@
+# Advent 2021 Day 5
+# Derek Kowaluk Dec 9, 2021
+# 
+
+import sys
+import os
+
+def get_example_data():
+	return [
+		"00100",
+		"11110",
+		"10110",
+		"10111",
+		"10101",
+		"01111",
+		"00111",
+		"11100",
+		"10000",
+		"11001",
+		"00010",
+		"01010"
+		]
+
+def get_data(filename):
+	f = None
+	data = []
+	try:
+		f = open(filename)
+	except OSError:
+		print("File Error")
+		return [-1, data]
+
+	line = f.readline()
+	while line:
+		data.append(line.strip())
+		line = f.readline()
+	return [0,data]
+
+
+def solve_part2(data, showoutput = True):
+	return 0
+
+def solve_part1(data, showoutput = True):
+	return 0
+
+def main():
+	data = None
+	if len(sys.argv) < 2: 
+		print("Using Example Data")
+		print("------------------")
+		data = get_example_data()
+	else:
+		filepath = sys.argv[1]
+		print("Using file:{}".format(filepath))
+		result,data = get_data(filepath)
+		if result : 
+			sys.exit()
+
+	solution = solve_part1(data, False)
+	print("The Solution for part 1 is {}".format(solution))
+
+	solution = solve_part2(data, False)
+	print("The Solution for part 2 is {}".format(solution))
+
+if __name__ == '__main__':
+	main()
