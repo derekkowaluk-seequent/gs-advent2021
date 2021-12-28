2e1596cdde36acd4748ade1ec8c6407d010371e3
Derek Kowaluk <derek.kowaluk@bentley.com>
2021-12-28 11:37:09 -0700

    Start Day 8

---
+++ day8/solutions/derekkowaluk/day8.py
@@ -0,0 +1,88 @@
+# Advent 2021 Day 8
+# Derek Kowaluk Dec 21, 2021
+# 
+
+import sys
+import os
+
+def get_example_data():
+	return """be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb |fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec |fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef |cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega |efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga |gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf |gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf |cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd |ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg |gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc |fgae cfgab fg bagce"""
+
+
+
+def get_data(filename):
+	f = None
+	data = []
+	try:
+		f = open(filename)
+	except OSError:
+		print("File Error")
+		return [-1, data]
+
+	line = f.readline()
+	while line:
+		data.append(line.strip())
+		line = f.readline()
+
+	return [0,data]
+
+def process_input(data):
+	print("Processing...")
+	
+	#[print(line.split('|')) for line in data]
+
+	sample = []
+	output = []
+	for eachline in data:
+		s, o = eachline.split('|')
+		sample.append(s)
+		output.append(o)
+	return [sample,output]
+
+
+def solve_part2(data, showoutput = True):
+	return 0
+
+def solve_part1(data, showoutput = True):
+	#print("sample:{}".format(data[0]))
+
+	print("Output:{}".format(data[1]))
+
+
+	
+	return 0
+
+def main():
+	data = None
+	if len(sys.argv) < 2: 
+		print("Using Example Data")
+		print("------------------")
+		data = get_example_data().splitlines()
+	else:
+		filepath = sys.argv[1]
+		print("Using file:{}".format(filepath))
+		result,data = get_data(filepath)
+		if result : 
+			sys.exit()
+
+	print("Data Size:{}".format(len(data)))
+	pdata = process_input(data)
+
+
+	solution = solve_part1(pdata, False)
+	print("The Solution for part 1 is {}".format(solution))
+
+	solution = solve_part2(pdata, False)
+	print("The Solution for part 2 is {}".format(solution))
+
+if __name__ == '__main__':
+	main()
